9753c15992da1f6fb920fcb697e44a09a32e9056
mitch <mitchellpalermo@icloud.com>
2022-07-21 12:25:45 -0500

    migrated attend a service page

---
+++ pages/attend.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import styles from "../src/css/attendService.module.scss";
+import PortableText from "@sanity/block-content-to-react";
+import { sanity } from "../src/util/index";
+import { serializers } from "../src/util/sanity-serializers";
+
+export async function getStaticProps() {
+  const query = `*[_type == "page" && title == "Attend a Service"]{
+    paragraphs,
+    faq,
+    scripture
+  }`;
+
+  const res = await sanity.fetch(query);
+  const data = res[0];
+  return { props: { data } };
+}
+export default function AttendService({ data }) {
+  const [pageData, setPageData] = useState(data);
+
+  const paragraphs = pageData?.paragraphs.map((paragraph) => (
+    <section>
+      <h4>{paragraph.paragraphTitle}</h4>
+      <PortableText blocks={paragraph.bodyText} serializers={serializers} />
+    </section>
+  ));
+
+  return (
+    <div className={styles.attend}>
+      <h1>
+        Attending a Service <br />
+        <span id={styles.highlight}>Verse by Verse Fellowship</span>
+      </h1>
+
+      <div className={styles.attendDesc}>{paragraphs}</div>
+      <p>
+        {" "}
+        For more information, please contact the church at info@vbvf.org or call
+        210-460-7556.
+      </p>
+    </div>
+  );
+}
