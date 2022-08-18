d8be86f4809ffd5bdadfb85f434722b81499fc3a
mitch <mitchellpalermo@icloud.com>
2022-08-18 18:16:32 -0500

    adding more to mens ministry page

--- pages/mens-ministry.js
+++ pages/mens-ministry.js
@@ -6,10 +6,13 @@ import ScriptureVerse from "../components/scripture-verse";
 import PortableText from "@sanity/block-content-to-react";
 import StaffInfo from "../components/staff-info";
 import Image from "next/image";
-import DigDeeper from "../public/images/mens-ministry/digging_compressed.jpeg";
+import Shovel from "../public/images/mens-ministry/shovel_compressed.jpeg";
 import Accelerate from "../public/images/mens-ministry/speedometer_compressed.jpeg";
-import Handshake from "../public/images/mens-ministry/handshake_compressed.jpeg";
+import HoldingBible from "../public/images/mens-ministry/holding-bible_compressed.jpg";
+import ClimbingStairs from "../public/images/mens-ministry/climbing-stairs.jpg";
+import SvgMask from "../public/images/mens-ministry/svg_mask.svg";
 import Link from "next/link";
+import Button from "../components/button";
 
 export async function getStaticProps() {
   const query = `*[_type == "page" && title == "Men's Ministry"]{
@@ -40,7 +43,7 @@ export default function MensMinistry({ data }) {
           reference={pageData.scripture.reference}
         />
       </div>
-      <div>maybe a video or carosel of images here?</div>
+
       <h2>Events</h2>
       <div className={`${styles.row} ${styles.events}`}>
         <Link href={"#dig-deeper"}>
@@ -48,10 +51,10 @@ export default function MensMinistry({ data }) {
             <h3>Dig Deeper</h3>
             <span className={styles.eventImage}>
               <div className={styles.imageContainer}>
-                <Image src={DigDeeper} />
+                <Image src={Shovel} />
               </div>
             </span>
-            Weekly
+            <h4>Weekly</h4>
           </div>
         </Link>
         <Link href={"#accelerate"}>
@@ -62,16 +65,16 @@ export default function MensMinistry({ data }) {
                 <Image src={Accelerate} />
               </div>
             </span>
-            Monthly
+            <h4>Monthly</h4>
           </div>
         </Link>
         <Link href={"#rally-up"}>
           <div className={styles.event}>
             <h3>Rally Up</h3>
             <span className={styles.eventImage}>
-              <Image src={Handshake} />
+              <Image src={HoldingBible} />
             </span>
-            Quarterly
+            <h4>Quarterly</h4>
           </div>
         </Link>
         <Link href={"#retreat"}>
@@ -79,23 +82,29 @@ export default function MensMinistry({ data }) {
             <h3>Retreat</h3>
             <span className={styles.eventImage}>
               <div className={styles.imageContainer}>
-                <Image src={Accelerate} />
+                <Image src={ClimbingStairs} />
               </div>
             </span>
-            Yearly
+            <h4>Yearly</h4>
           </div>
         </Link>
       </div>
       <div>
-        <div
-          id="dig-deeper"
-          className={`${styles.fullWidthRow} ${styles.digDeeper}`}
-        >
-          <h4>Dig Deeper</h4>
-          <p>
-            Weekly, IRON MEN gather to share a meal, pray, and dig deeper into
-            God’s Word through focused study of the Bible.
-          </p>
+        <div id="dig-deeper" className={styles.fullWidthRow}>
+          <div className={styles.eventDetails}>
+            <h4>Dig Deeper</h4>
+            <p>
+              Weekly, IRON MEN gather to share a meal, pray, and dig deeper into
+              God’s Word through focused study of the Bible.
+            </p>
+            <Button
+              title="See details"
+              link="https://vbvf.churchcenter.com/registrations/events/category/18785"
+              size="medium"
+              color="green"
+            />
+          </div>
+          <div className={`${styles.imageFeature} ${styles.digDeeper}`} />
         </div>
         <div
           id="accelerate"
@@ -106,6 +115,12 @@ export default function MensMinistry({ data }) {
             Accelerate creates monthly opportunities for men of like interest to
             gather and strengthen relationships through fellowship and service.
           </p>
+          <Button
+            title="RSVP for Accelerate"
+            link="https://vbvf.churchcenter.com/registrations/events/category/18785"
+            size="medium"
+            color="bone"
+          />
         </div>
         <div
           id="rally-up"
@@ -116,6 +131,12 @@ export default function MensMinistry({ data }) {
             This quarterly event is a time of corporate worship, which combines
             fellowship, study and service.
           </p>
+          <Button
+            title="Get Rally Up info"
+            link="https://vbvf.churchcenter.com/registrations/events/category/18785"
+            size="medium"
+            color="bone"
+          />
         </div>
         <div
           id="retreat"
@@ -127,6 +148,12 @@ export default function MensMinistry({ data }) {
             routine to reflect, study and praise God’s provision in your life in
             a serene setting.
           </p>
+          <Button
+            title="Register for retreat"
+            link="https://vbvf.churchcenter.com/registrations/events/category/18785"
+            size="medium"
+            color="bone"
+          />
         </div>
       </div>
       {pageData.ministryLeader && (
